55372bfc6daee25c733e74b7d8474cf7adc7683a
damagr <damiamartinez94@gmail.com>
2021-02-01 20:36:25 +0100

    Merge de la rama articles

---
+++ WebAPI/Controllers/ArticlesController.cs
@@ -0,0 +1,43 @@
+﻿using GPE.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace GPE.Controllers
+{
+    [RoutePrefix("api/Articles")]
+    public class ArticlesController : ApiController
+    {
+        // GET: api/Article
+        [Route(""), HttpGet]
+        public IEnumerable<Article> Get()
+        {
+            var repo = new ArticlesRepository();
+            List<Article> articles = repo.Retrieve();
+            return articles;
+        }
+
+        // POST: api/Article
+        [Route(""), HttpPost]
+        public void Post(Article article)
+        {
+            var repo = new ArticlesRepository();
+            repo.Save(article);
+        }
+
+        // PUT: api/Article/ID_Number
+        [Route(""), HttpPut]
+        public void Put(int id)
+        {
+            var repo = new ArticlesRepository();
+            repo.Update(id);
+        }
+
+        // PUT: api/Articles/
+        [Route(""), HttpPut]
+        public void Put([FromBody] Article article)
+        {
+            var repo = new ArticlesRepository();
+            repo.Update(article);
+        }
+    }
+}
