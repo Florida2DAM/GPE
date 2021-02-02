4e9c6f05e5746711913f9d72081e78b0d9970168
damagr <damiamartinez94@gmail.com>
2021-02-02 13:12:42 +0100

    Volvemos a crear el proyecto debido a las incidencias que han surgido al empezar a trabajar con él.

--- GPE/ios/GPETests/GPETests.m
+++ GPE/ios/GPETests/GPETests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface GPETests : XCTestCase
+@interface gpeTests : XCTestCase
 
 @end
 
-@implementation GPETests
+@implementation gpeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
