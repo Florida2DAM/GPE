bab5704d0bc9d8876a307693abfc3cdc765bdd02
damagr <damiamartinez94@gmail.com>
2021-02-02 19:39:21 +0100

    Devolvemos la pantalla en la que hemos hecho las pruebas a su estado inicial

--- GPE/screens/MainScreen.js
+++ GPE/screens/MainScreen.js
@@ -1,28 +1,14 @@
 'use strict';
 
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import {PaymentPicker} from '../components/PaymentPicker';
+import {View} from "react-native";
 
 const style = require('../components/Styles');
 
 export default class MainScreen extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            paymentMethod: '',
-        };
-    }
-
-    getPaymentMethod = (e) => {
-        this.setState({paymentMethod: e});
-    };
-
     render() {
         return (
-            <View style={[style.container, style.flexColumnCenter]}>
-                <PaymentPicker getPaymentMethod={this.getPaymentMethod}/>
+            <View style={style.container}>
             </View>
         );
     }
