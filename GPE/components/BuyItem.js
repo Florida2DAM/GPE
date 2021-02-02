8e3347c0c4451f43f95bcd56f14b2763fe9ed11b
Miguel Navarro Travé <otracuentaderrellenomas@gmail.com>
2021-02-02 16:41:01 +0100

    Estilo base creado

--- GPE/components/BuyItem.js
+++ GPE/components/BuyItem.js
@@ -12,18 +12,18 @@ export class BuyItem extends Component {
         return (
             <View style={styles.bet}>
                 <View style={{ flex: 1 }}>
-                    <Text>Item name</Text>
-                    <Text>ID: 1</Text>
-                    <Text>Price: 10€</Text>
-                    <Button title='Remove'></Button>
+                    <Text style={styles.text}>Item name</Text>
+                    <Text style={[styles.text, styles.smallText]}>ID: 1</Text>
+                    <Text style={[styles.text, styles.smallText]}>Price: 10€</Text>
+                    <Button title='Remove' type='clear' titleStyle={styles.button}></Button>
                 </View>
-                <View style={{ flex: 1 }}>
+                <View style={{ flex: 1, justifyContent: 'space-between', alignItems: 'flex-end'}}>
                     <View style={{flexDirection: 'row'}}>
-                        <Button title='-'></Button>
-                        <Text>2</Text>
-                        <Button title='+'></Button>
+                        <Button title='-' type='clear' titleStyle={styles.button}></Button>
+                        <Text style={styles.text}>1</Text>
+                        <Button title='+' type='clear' titleStyle={styles.button}></Button>
                     </View>
-                    <Text>Total: 20€</Text>
+                    <Text style={styles.text}>Total: 20€</Text>
                 </View>
             </View>
         );
@@ -32,14 +32,28 @@ export class BuyItem extends Component {
 
 const styles = StyleSheet.create({
     bet: {
-        borderWidth: 1,
+        borderWidth: 2,
         borderRadius: 10,
-        backgroundColor: '#E6D6D3',
-        height: 125,
-        width: '80%',
+        borderColor: '#ffcc57',
+        backgroundColor: '#2a2a2a',        
+        height: '20%',
+        width: '90%',
         alignSelf: 'center',
-        marginTop: 13,
-        marginBottom: 7,
-        flexDirection: 'row',
+        flexDirection: 'row',       
+        paddingTop: '3%',
+        paddingBottom: '4%',
+        paddingLeft: '4%',
+        paddingRight: '4%',
     },
+    text: {
+        color: '#f7f7f7',
+        fontSize: 24
+    },
+    smallText: {
+        fontSize: 18
+    },
+    button: {
+        color: '#ffcc57',
+        fontSize: 28
+    }
 });
