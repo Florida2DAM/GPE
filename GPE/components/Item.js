80bd0f737843a9a26b7ef3d71ef35f0c20da0a27
jesagi <xusoop3@gmail.com>
2021-02-02 18:15:40 +0100

    FE-C Item desarollado

---
+++ GPE/components/Item.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
+import { Icon } from 'react-native-elements';
+
+const AddItemToCarrito = () => {
+    console.log("Furula");//Cambiar por funcion para abrir el componente de añadir items
+} 
+
+export class Item extends Component {
+
+    render() {
+        return (
+            <View style={styles.item}>
+                <View style={styles.text}>
+                    <View style={styles.info}>
+                        <Text style={styles.text}>NAME</Text>
+                    </View>
+                    <View style={styles.info}>
+                        <Text style={styles.text}>ID</Text>
+                    </View>
+                </View>
+                <View style={styles.icon}>
+                    <Icon
+                        name='add'
+                        type='material'
+                        color='#ffcc57'
+                        size={55}
+                        onPress={AddItemToCarrito} />
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    item: {
+        borderColor: "#ffcc57",
+        borderTopWidth: 2,
+        borderBottomWidth: 2,
+        height: "12%",
+        flexDirection: "row"
+    },
+    icon: {
+        flex: 1,
+        flexDirection: "column",
+        alignSelf: "center",
+        alignItems: "flex-end",
+        paddingRight: "5%",
+    },
+    info: {
+        flex: 1,
+    },
+    text: {
+        flex: 1,
+        fontSize: 20,
+        color: "#f7f7f7",
+        textAlignVertical: "center",
+        paddingLeft: "5%",
+    }
+
+})
