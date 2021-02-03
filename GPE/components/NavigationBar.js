6c7fa06b30709ba9a156b9acd35d8b44c1eec277
jesagi <jesagi@floridauniversitaria.es>
2021-02-03 16:58:36 +0100

    Barra de navegacion desarollada

--- GPE/components/NavigationBar.js
+++ GPE/components/NavigationBar.js
@@ -1,28 +1,43 @@
+/* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
 import { StyleSheet, ScrollView, View, Text } from 'react-native';
 import { Icon } from "react-native-elements";
+
+const pressLeftIcon = () =>{
+    console.log("Click al de la otra derecha")
+}
+
+const pressRightIcon = () =>{
+    console.log("Click al de la otra Izquierda")
+}
+
+// NOMBRES DE LOS ICONOS:
+// Izquierda: "navigate-before" flecha atras.  "" dejarlo en blanco
+// Derecha: "navigate-next" felcha adelante. "add" sgno suma. "" dejarlo en blanco
 
 export class NavigationBar extends Component {
     render() {
         return (
             <View>
                 <View style={styles.item}>
-                    <View style={styles.icon}>
+                    <View style={[styles.part, {alignItems: "flex-start"}]}>
                         <Icon
-                            name='add'
+                            name={this.props.lIcon}
                             type='material'
                             color='#ffcc57'
-                            size={55} />
+                            size={70}
+                            onPress={pressLeftIcon} />
                     </View>
-                    <View style={styles.icon}>
-                        <Text>T.Pagina</Text>
+                    <View style={styles.part}>
+                        <Text style={[styles.text, {textAlign:"center"}]}>{this.props.pageName}</Text>
                     </View>
-                    <View style={styles.icon}>
+                    <View style={[styles.part, {alignItems: "flex-end"}]}>
                         <Icon
-                            name='add'
+                            name={this.props.rIcon}
                             type='material'
                             color='#ffcc57'
-                            size={55} />
+                            size={70}
+                            onPress={pressRightIcon} />
                     </View>
                 </View>
             </View>
@@ -31,11 +46,16 @@ export class NavigationBar extends Component {
 }
 const styles = StyleSheet.create({
     item: {
-        borderColor: "#ffcc57",
-        borderTopWidth: 2,
-        borderBottomWidth: 2,
         height: "30%",
         flexDirection: "row",
+    },
+    part: {
+        flex: 1,
+        alignSelf: "center",
+    },
+    text: {
+        fontSize: 25,
+        color: "#f7f7f7",
     },
 
 })
