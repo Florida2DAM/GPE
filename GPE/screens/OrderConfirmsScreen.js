77114bb8d6ce4cb26d728fb26e162b6be487c16d
damagr <damiamartinez94@gmail.com>
2021-02-04 23:48:32 +0100

    Corrección de nomenclaturas.

--- GPE/screens/OrderConfirmsScreen.js
+++ GPE/screens/OrderConfirmsScreen.js
@@ -1,49 +1,50 @@
 'use strict';
 
-import React, { Component } from 'react';
-import { FlatList, ScrollView, View } from 'react-native';
-import { ModifyQuantity } from '../components/ModifyQuantity';
-import { NavigationBar } from '../components/NavigationBar';
-import { GPEFilter } from '../components/GPEFilter';
-import { ContactInfo } from '../components/ContactInfo';
-import { Divider } from 'react-native-elements';
-import { GPELabel } from '../components/GPELabel';
+import React, {Component} from 'react';
+import {FlatList, View} from 'react-native';
+import {ModifyQuantity} from '../components/ModifyQuantity';
+import {NavigationBar} from '../components/NavigationBar';
+import {ContactInfo} from '../components/ContactInfo';
+import {Divider} from 'react-native-elements';
+import {GPELabel} from '../components/GPELabel';
 
 const style = require('../components/Styles');
 
 export default class OrderConfirmsScreen extends Component {
     state = {
-        name: "WEI Luo",
-        dni: "Y18273678",
-        productos: [
-            { name: "CUlo", id: 2, price: 123 },
+        name: 'WEI Luo',
+        dni: 'Y18273678',
+        products: [
+            {name: 'CUlo', id: 2, price: 123},
 
-            { name: "CUlo", id: 2, price: 123 },
-        ]
-    }
+            {name: 'CUlo', id: 2, price: 123},
+        ],
+    };
+
     render() {
         return (
             <View style={style.container}>
                 <View>
-                    <NavigationBar leftIcon={'arrow-back-ios'} leftIconSize={40} pageName={'Confirm'} rightIcon={'check'} rightIconSize={48}></NavigationBar>
+                    <NavigationBar leftIcon={'arrow-back-ios'} leftIconSize={40} pageName={'Confirm'}
+                                   rightIcon={'check'} rightIconSize={48}/>
                 </View>
-                <ContactInfo name={"WEI Luo"} dni="w12321432"></ContactInfo>
-                <Divider style={{ height: 10, backgroundColor: "none" }} />
+                <ContactInfo name={'WEI Luo'} dni="w12321432"/>
+                <Divider style={{height: 10, backgroundColor: 'none'}}/>
 
                 <FlatList
-                    data={this.state.productos}
+                    data={this.state.products}
                     keyExtractor={(item, index) => index.toString()}
-                    renderItem={({ item }) => {
+                    renderItem={({item}) => {
                         return (
-
-                            <View style={{ flex: 1 }}>
-                                <ModifyQuantity name={item.name} price={item.price} id={item.id} />
+                            <View style={{flex: 1}}>
+                                <ModifyQuantity name={item.name} price={item.price} id={item.id}/>
                             </View>
                         );
                     }}
                 />
-                <View style={{alignItems:'center'}}>
-                <GPELabel title="Total: "paddingLeft={'2%'} width={"50%"} marginBottom={'4%'} content="15000" currency='€'/>
+                <View style={{alignItems: 'center'}}>
+                    <GPELabel title="Total: " paddingLeft={'2%'} width={'50%'} marginBottom={'4%'} content="15000"
+                              currency='€'/>
                 </View>
             </View>
         );
