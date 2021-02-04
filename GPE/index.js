02f5651ca21c6be5d44bf125a4ca1d8e9275273d
damagr <damiamartinez94@gmail.com>
2021-02-04 10:40:26 +0100

    NoTicket

--- GPE/index.js
+++ GPE/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import OrderArticlesScreen from './screens/OrderArticlesScreen';
+import SettingsScreen from './screens/SettingsScreen';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => OrderArticlesScreen);
+AppRegistry.registerComponent(appName, () => SettingsScreen);
