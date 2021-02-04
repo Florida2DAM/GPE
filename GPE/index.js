13680285ee1172f220caa53fd32f926ace975ad6
damagr <damiamartinez94@gmail.com>
2021-02-04 10:53:52 +0100

    Corregidas las pantallas que utilizan el gpepicker indicando el tamaño por propiedad.

--- GPE/index.js
+++ GPE/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import DeliverPaymentScreen from './screens/DeliverPaymentScreen';
+import OrderArticlesScreen from './screens/OrderArticlesScreen';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => DeliverPaymentScreen);
+AppRegistry.registerComponent(appName, () => OrderArticlesScreen);
