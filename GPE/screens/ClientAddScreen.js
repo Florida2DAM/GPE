7e69991d1793b460f888eea2b6ad8664a07af1bb
jesagi <xusoop3@gmail.com>
2021-02-04 16:54:30 +0100

    Pantalla dearolada

--- GPE/screens/ClientAddScreen.js
+++ GPE/screens/ClientAddScreen.js
@@ -1,14 +1,58 @@
 'use strict';
 
-import React, {Component} from 'react';
-import {View} from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { NavigationBar } from "../components/NavigationBar";
+import { GPEInput } from "../components/GPEInput";
+
 
 const style = require('../components/Styles');
 
-export class ClientAddScreen extends Component {
+export default class ClientAddScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            surname: '',
+            barName: '',
+            nif: '',
+            phone: '',
+            location: '',
+        }
+    }
+
+    getName = (n) => {
+        this.setState({ name: n });
+    };
+    getSurname = (s) => {
+        this.setState({ surname: s })
+    };
+    getBarName = (b) => {
+        this.setState({ barName: b })
+    };
+    getNif = (n) => {
+        this.setState({ nif: n })
+    };
+    getPhone = (p) => {
+        this.setState({ phone: p })
+    };
+    getLocation = (l) => {
+        this.setState({ location: l })
+    };
+
     render() {
         return (
             <View style={style.container}>
+                <NavigationBar leftIcon={"navigate-before"} leftIconSize={50} pageName={"Add Client"} rightIcon={"done"} rightIconSize={50} />
+                <View style={{alignItems: 'center'}}>
+                    <GPEInput title={'Name'} placeholder={'example name'} width='90%' height={5} marginTop='10%' getValue={this.getName} />
+                    <GPEInput title={'Surname'} placeholder={'example surname'} width='90%' height={5} marginTop='5%' getValue={this.getSurname} />
+                    <GPEInput title={'Bar Name'} placeholder={'example bar name'} width='90%' height={5} marginTop='5%' getValue={this.getBarName} />
+                    <GPEInput title={'NIF/NIE'} placeholder={'example nif/nie'} width='90%' height={5} marginTop='5%' getValue={this.getNif} />
+                    <GPEInput title={'Phone number'} placeholder={'example phone number'} width='90%' height={5} marginTop='5%' getValue={this.getPhone} />
+                    <GPEInput title={'Location'} placeholder={'example location'} width='90%' height={5} marginTop='5%' getValue={this.getLocation} />
+                </View>
             </View>
         );
     }
