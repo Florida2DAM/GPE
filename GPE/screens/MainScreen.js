c2f7c4838e4c29f7994ddea26681a3b930f260ac
jesagi <jesagi@floridauniversitaria.es>
2021-02-04 13:13:28 +0100

    stack navigation dependencies

--- GPE/screens/MainScreen.js
+++ GPE/screens/MainScreen.js
@@ -1,7 +1,11 @@
+/* eslint-disable prettier/prettier */
+import 'react-native-gesture-handler';
 'use strict';
 
 import React, {Component} from 'react';
 import {View} from "react-native";
+import { NavigationBar } from '../components/NavigationBar';
+import{ NavigationContainer} from'@react-navigation/native';
 
 const style = require('../components/Styles');
 
@@ -9,6 +13,7 @@ export default class MainScreen extends Component {
     render() {
         return (
             <View style={style.container}>
+                <NavigationBar lIcon={"navigate-before"} pageName={"Clients"} rIcon={"add"}></NavigationBar>
             </View>
         );
     }
