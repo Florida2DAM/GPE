4cb77dad70c8b2fb9b45657c87ac71a29cded2e6
damagr <damiamartinez94@gmail.com>
2021-02-05 18:58:30 +0100

    Modificado el indentado

--- GPE/screens/DeliverPaymentScreen.js
+++ GPE/screens/DeliverPaymentScreen.js
@@ -4,6 +4,8 @@ import React, {Component} from 'react';
 import {View} from 'react-native';
 import {GPEPicker} from '../components/GPEPicker';
 import {GPEInput} from '../components/GPEInput';
+import {GPELabel} from '../components/GPELabel';
+
 const style = require('../components/Styles');
 export default class DeliverPaymentScreen extends Component {
 
@@ -11,10 +13,10 @@ export default class DeliverPaymentScreen extends Component {
         super();
         this.state = {
             paymentMethod: '',
-            total:0,
-            paid:0,
+            total: 0,
+            paid: 0,
             contractName: '',
-            NIF:'',
+            NIF: '',
         };
     }
 
@@ -38,15 +40,19 @@ export default class DeliverPaymentScreen extends Component {
         return (
             <View style={[style.container, style.flexColumnCenter]}>
 
-                 <NavigationBar leftIcon={'navigate-before'} leftIconSize={50} pressLeftIcon={this.onPressLeftIcon}
+                <NavigationBar leftIcon={'navigate-before'} leftIconSize={50} pressLeftIcon={this.onPressLeftIcon}
                                pageName={'Payment'} rightIcon={'done'} rightIconSize={50}
                                pressRightIcon={this.onPressRightIcon}/>
 
-                <GPEInput  title={'Total'} placeholder={'0.0€'} width='80%' height={5} marginTop='10%'   getValue={this.getTotal} />
-                <GPEInput  title={'Paid'} placeholder={'0.0€'}  width='80%' height={5} marginTop='10%'  getValue={this.getPaid}/>
+                <GPELabel title={'Total'} content={'0.0€'} width='80%' height={5} marginTop='10%'
+                          getValue={this.getTotal}/>
+                <GPEInput title={'Paid'} placeholder={'0.0€'} width='80%' height={5} marginTop='10%'
+                          getValue={this.getPaid}/>
 
-                <GPEInput title={'Contract Name'} placeholder={'example name'} width='80%' height={5} marginTop='10%'  getValue={this.getName}/>
-                <GPEInput title={'NIF'} placeholder={'3236273'}  width='80%' height={5} marginTop='10%' marginBottom='10%'  getValue={this.getNIF} />
+                <GPEInput title={'Contract Name'} placeholder={'example name'} width='80%' height={5} marginTop='10%'
+                          getValue={this.getName}/>
+                <GPEInput title={'NIF'} placeholder={'3236273'} width='80%' height={5} marginTop='10%'
+                          marginBottom='10%' getValue={this.getNIF}/>
                 <GPEPicker sendIcon={'payment'} pickerSize={'62%'} getOption={this.getPaymentMethod}/>
 
             </View>
