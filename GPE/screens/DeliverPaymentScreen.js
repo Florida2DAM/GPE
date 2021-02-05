efb0ec028d70cfc73fd04ece501d2062a763938c
vicentsargues <vicentsargues@gmail.com>
2021-02-05 16:53:21 +0100

    fixed

--- GPE/screens/DeliverPaymentScreen.js
+++ GPE/screens/DeliverPaymentScreen.js
@@ -41,10 +41,10 @@ export default class DeliverPaymentScreen extends Component {
                  <NavigationBar leftIcon={'navigate-before'} leftIconSize={50} pressLeftIcon={this.onPressLeftIcon}
                                pageName={'Payment'} rightIcon={'done'} rightIconSize={50}
                                pressRightIcon={this.onPressRightIcon}/>
-                <View style={[style.flexRowCenter]}>
-                <GPEInput  title={'Total'} placeholder={'0.0€'} width='39%' height={2.5} marginTop='10%'marginRight='2%'  getValue={this.getTotal} />
-                <GPEInput  title={'Paid'} placeholder={'0.0€'}  width='39%' height={2.5} marginTop='10%' marginLeft='2%' marginR='30%'  getValue={this.getPaid}/>
-                </View>
+
+                <GPEInput  title={'Total'} placeholder={'0.0€'} width='80%' height={5} marginTop='10%'   getValue={this.getTotal} />
+                <GPEInput  title={'Paid'} placeholder={'0.0€'}  width='80%' height={5} marginTop='10%'  getValue={this.getPaid}/>
+
                 <GPEInput title={'Contract Name'} placeholder={'example name'} width='80%' height={5} marginTop='10%'  getValue={this.getName}/>
                 <GPEInput title={'NIF'} placeholder={'3236273'}  width='80%' height={5} marginTop='10%' marginBottom='10%'  getValue={this.getNIF} />
                 <GPEPicker sendIcon={'payment'} pickerSize={'62%'} getOption={this.getPaymentMethod}/>
