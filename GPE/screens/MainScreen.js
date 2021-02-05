08be7f68fdff4e95cb806260d7cbd40df10d2205
jesagi <jesagi@floridauniversitaria.es>
2021-02-05 19:41:30 +0100

    jiji no se rompe

--- GPE/screens/MainScreen.js
+++ GPE/screens/MainScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
