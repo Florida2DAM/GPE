c6e60c0399fad0f58395c6a7ca6c2d05627287f3
damagr <damiamartinez94@gmail.com>
2021-02-06 18:25:27 +0100

    NoTicket

--- GPE/screens/SettingsScreen.js
+++ GPE/screens/SettingsScreen.js
@@ -4,7 +4,7 @@ import {GPEPicker} from '../components/GPEPicker';
 import {NavigationBar} from '../components/NavigationBar';
 import {GPELabel} from '../components/GPELabel';
 import {GPELogo} from '../components/GPELogo';
-import {GPEApi, axios, style} from '../components/GPEConst'
+import {GPEApi, axios, style} from '../components/GPEConst';
 
 export default class SettingsScreen extends Component {
     constructor() {
