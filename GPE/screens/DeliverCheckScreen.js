12cff7bebe66a929d3f091515f99afc0c6012881
damagr <damiamartinez94@gmail.com>
2021-02-08 00:54:56 +0100

    Simplicamos el código, eliminamos la llamada axios pasando el parámetro de la pantalla anterior que ya contiene toda la información relacionada con el pedido.

--- GPE/screens/DeliverCheckScreen.js
+++ GPE/screens/DeliverCheckScreen.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {FlatList, View} from 'react-native';
 import {NavigationBar} from '../components/NavigationBar';
-import {axios, GPEApi, style} from '../components/GPEConst';
+import {style} from '../components/GPEConst';
 import {ArticleCard} from '../components/ArticleCard';
 
 export default class DeliverCheckScreen extends Component {
@@ -14,20 +14,8 @@ export default class DeliverCheckScreen extends Component {
     }
 
     componentDidMount() {
-        this.setState({order: this.props.route.params.item}, () => {
-            this.getOrderLines();
-        });
+        this.setState({order: this.props.route.params.item});
     }
-
-    getOrderLines = () => {
-        let newLines = [];
-        axios.get(GPEApi + 'OrderLines?orderId=' + this.state.order.OrderId).then((response) => {
-            response.data.forEach(item => {
-                newLines.push(item);
-            });
-            this.setState({orderLines: newLines});
-        });
-    };
 
     render() {
         return (
@@ -38,7 +26,7 @@ export default class DeliverCheckScreen extends Component {
                                pressRightIcon={() => this.props.navigation.navigate('DeliverPaymentScreen', {item: this.state.order})}/>
                 <View style={{marginTop: '5%'}}>
                     <FlatList
-                        data={this.state.orderLines}
+                        data={this.state.order.OrderLines}
                         keyExtractor={(item, index) => index.toString()}
                         renderItem={({item}) => {
                             return (
