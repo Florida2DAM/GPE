55bd6ddd3b931f96ba5cb2a76be66ec210be34fd
jesagi <jesagi@floridauniversitaria.es>
2021-02-07 18:03:31 +0100

    Error client card

--- GPE/screens/VisitSalesScreen.js
+++ GPE/screens/VisitSalesScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 'use strict';
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
@@ -6,8 +7,7 @@ import ClientCard from '../components/ClientCard';
 import {NavigationBar} from '../components/NavigationBar';
 import {GPEFilter} from '../components/GPEFilter';
 import {Divider} from 'react-native-elements';
-
-const style = require('../components/Styles');
+import {axios, GPEApi, style} from '../components/GPEConst';
 
 export default class VisitSalesScreen extends Component {
 
@@ -15,133 +15,27 @@ export default class VisitSalesScreen extends Component {
         super(props);
         this.state = {
             employeeType: this.props.employeeType,
-            ClientData: [
-                {
-                    id: 1,
-                    name: 'Lemon Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Wei Luo',
-                },
-                {
-                    id: 2,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 3,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 4,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 5,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 6,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 7,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 8,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 9,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 10,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 11,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-            ],
+            ClientData: [],
             visible: true,
 
         };
     }
+    componentDidMount() {
+        this.getClient();
+    }
+
+    getClient = () => {
+        let client = [];
+
+        axios.get(GPEApi + 'Clients').then((response) => {
+            response.data.forEach(info => {
+                client.push(info);
+                console.log(info);
+            });
+            this.setState({ClientData: client});
+            console.log(this.state.ClientData);
+        });
+    };
 
     invisible = () => {
         this.setState({visible: false});
@@ -166,21 +60,21 @@ export default class VisitSalesScreen extends Component {
                         <Divider style={{height: 10, backgroundColor: 'none'}}/>
                         <FlatList
                             data={this.state.ClientData}
-                            keyExtractor={(item) => item.id.toString()}
+                            keyExtractor={(item) => item.ClientId.toString()}
                             renderItem={({item}) => {
                                 return (
                                     <Pressable
                                         onPress={() => this.props.navigation.navigate('OrderArticlesScreen', {client: item})}>
                                         <ClientCard
-                                            id={item.id}
-                                            name={item.name}
-                                            address={item.address}
-                                            city={item.city}
-                                            country={item.country}
-                                            province={item.province}
-                                            contactName={item.contactName}
-                                            phone={item.phone}
-                                            codePostal={item.codePostal}
+                                            id={item.ClientId}
+                                            name={item.Name}
+                                            address={item.Address}
+                                            city={item.City}
+                                            country={item.Country}
+                                            province={item.Province}
+                                            contactName={item.ContactName}
+                                            phone={item.Phone}
+                                            codePostal={item.PostalCode}
                                         />
                                     </Pressable>
                                 );
