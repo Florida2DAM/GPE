469d0aa0894d8986512e5c37b77d720d543e3a26
damagr <damiamartinez94@gmail.com>
2021-02-07 18:11:36 +0100

    NoTicket

--- GPE/screens/VisitDeliverScreen.js
+++ GPE/screens/VisitDeliverScreen.js
@@ -1,189 +1,55 @@
-'use strict';
-
 import React, {Component} from 'react';
 import {FlatList, Pressable, View} from 'react-native';
 import ClientCard from '../components/ClientCard';
 import {NavigationBar} from '../components/NavigationBar';
 import {GPEFilter} from '../components/GPEFilter';
-import {Divider} from 'react-native-elements';
-
-const style = require('../components/Styles');
+import {axios, GPEApi, style} from '../components/GPEConst';
 
 export default class VisitDeliverScreen extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            ClientData: [
-                {
-                    id: 1,
-                    name: 'Lemon Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Wei Luo',
-                },
-                {
-                    id: 2,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 3,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 4,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 5,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 6,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 7,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 8,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 9,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 10,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-                {
-                    id: 11,
-                    name: 'Sanwhich Bar',
-                    address: 'Calle Valencia 19',
-                    city: 'Valencia',
-                    province: 'Valencia',
-                    country: 'España',
-                    codePostal: '46480',
-                    phone: '123456789',
-                    contactName: 'Jesus',
-                },
-            ],
-            visible: true,
+            orders: [],
+            employee: 'Jesus',
         };
     }
 
-    invisible = () => {
-        this.setState({visible: false});
-    };
-    visible = () => {
-        this.setState({visible: true});
+    componentDidMount() {
+        this.getOrders();
+    }
+
+    getOrders = () => {
+        axios.get(GPEApi + 'Orders/GetDeliver').then((response) => {
+            response.data.forEach(item => {
+                if (item.Deliverer === this.state.employee) {
+                    this.setState({orders: response.data});
+                }
+            });
+        });
     };
 
     render() {
         return (
             <>
-                <View style={[style.container, {flex: 1}]}>
-                    <NavigationBar leftIcon={'arrow-back-ios'} leftIconSize={40} pageName={'Orders'}/>
-                    <GPEFilter onFocus={this.invisible} onBlur={this.visible}/>
+                <View style={style.container}>
+                    <NavigationBar leftIcon={'arrow-back-ios'} leftIconSize={40} pageName={'Orders'}
+                                   pressLeftIcon={() => this.props.navigation.goBack()}/>
+                    <GPEFilter/>
+                    <FlatList
+                        data={this.state.orders}
+                        keyExtractor={(item) => item.OrderId.toString()}
+                        renderItem={({item}) => {
+                            return (
+                                <Pressable
+                                    onPress={() => this.props.navigation.navigate('DeliverCheckScreen')}>
+                                    <ClientCard
+                                        client={item}
+                                    />
+                                </Pressable>
+                            );
+                        }}/>
                 </View>
-
-                {this.state.visible ? <View style={[style.container, {flexDirection: 'column', flex: 5}]}>
-                        <Divider style={{height: 10, backgroundColor: 'none'}}/>
-                        <FlatList
-                            data={this.state.ClientData}
-                            keyExtractor={(item) => item.id.toString()}
-                            renderItem={({item}) => {
-                                return (
-                                    <Pressable
-                                        onPress={() => this.props.navigation.navigate('DeliverCheckScreen')}>
-                                        <ClientCard
-                                            id={item.id}
-                                            name={item.name}
-                                            address={item.address}
-                                            city={item.city}
-                                            country={item.country}
-                                            province={item.province}
-                                            contactName={item.contactName}
-                                            phone={item.phone}
-                                            codePostal={item.codePostal}
-                                        />
-                                    </Pressable>
-                                );
-                            }}
-                        />
-                    </View> :
-                    <View/>}
             </>
         );
     }
-
 }
