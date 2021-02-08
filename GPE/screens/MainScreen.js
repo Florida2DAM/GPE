b3235a9e81da8f2e1fd7b8dc08134e118be2218f
vicentsargues <vicentsargues@gmail.com>
2021-02-08 16:53:47 +0100

    fixing

--- GPE/screens/MainScreen.js
+++ GPE/screens/MainScreen.js
@@ -1,56 +1,82 @@
 
+
+/* eslint-disable prettier/prettier */
+import 'react-native-gesture-handler';
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {View} from 'react-native';
 import {GPEButton} from '../components/GPEButton';
-import {SeparatorLine} from '../components/SeparatorLine';
+import {GPELogo} from '../components/GPELogo';
+import ClientAddScreen from './ClientAddScreen';
+import DeliverPaymentScreen from './DeliverPaymentScreen';
+import ItemsListScreen from './ItemsListScreen';
+import OrderAddItemsScreen from './OrderAddItemsScreen';
+import OrderArticlesScreen from './OrderArticlesScreen';
+import OrderConfirmsScreen from './OrderConfirmsScreen';
+import SettingsScreen from './SettingsScreen';
+import VisitDeliverScreen from './VisitDeliverScreen';
+import VisitSalesScreen from './VisitSalesScreen';
+import ClientsListScreen from './ClientsListScreen';
+import DeliverCheckScreen from './DeliverCheckScreen';
 
 const style = require('../components/Styles');
+const stack = createStackNavigator();
 
 export default class MainScreen extends Component {
+    constructor() {
+        super();
+        this.state = {
+             // employeeType: 'salesMan',
+            employeeType: 'deliveryMan',
+        };
+    }
 
-    alert = (e) => {
-        console.log('Function' + e);
-        alert(e);
-    };
-
-    render() {
+    mainScreen = ({navigation}) => {
         return (
             <View style={style.container}>
-                <View style={{flex: 2, alignItems: 'center'}}>
-                    <Text style={{fontSize: 37, color: 'white'}}>Pagina Principal</Text>
-                    <SeparatorLine/>
+                <View style={{marginLeft: '5%', marginRight: '5%'}}>
+                    <View style={{marginTop: '20%', marginBottom: '20%', marginRight: '10%', marginLeft: '10%'}}>
+                        <GPELogo/>
+                    </View>
                 </View>
-
-                <View style={{
-                    margin: '10%',
-                    flex: 3,
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}>
-                    <View style={{flex: 1}}>
-                        <GPEButton iconName="local-shipping" iconSize={60} buttonName="Visit"
-                                   callback={() => this.alert('hola')}/>
-                        <GPEButton iconName="local-shipping" iconSize={60} buttonName="Visit"
-                                   callback={() => this.alert('hola')}/>
-                    </View>
-                    <View style={{flex: 1}}>
-                        <GPEButton/>
-                        <GPEButton/>
-                    </View>
-
                 <View style={[style.flexRowCenter, {justifyContent: 'space-evenly', marginTop: '5%'}]}>
                     <GPEButton iconName='local-shipping' iconSize={60} buttonName='VISIT'
                                onPress={this.state.employeeType === 'salesMan' ? () => navigation.navigate('VisitSalesScreen') : () => navigation.navigate('VisitDeliverScreen')}/>
                     <GPEButton iconName='contact-page' iconSize={60} buttonName='CLIENTS'
-                               onPress={() => navigation.navigate('ClientAddScreen')}/>
+                               onPress={() => navigation.navigate('ClientsListScreen', {
+                                   id: '2',
+                                   employeeType: this.state.employeeType,
+                               })}/>
                 </View>
-                <View style={{flex: 2}}>
-
+                <View style={[style.flexRowCenter, {justifyContent: 'space-evenly', marginTop: '5%'}]}>
+                    <GPEButton iconName='category' iconSize={60} buttonName='ITEMS'
+                               onPress={() => navigation.navigate('ItemsListScreen')}/>
+                    <GPEButton iconName='settings' iconSize={60} buttonName='SETTINGS'
+                               onPress={() => navigation.navigate('SettingsScreen')}/>
                 </View>
+            </View>
+        );
+    };
 
-            </View>
-            </View>
+    render() {
+        return (
+            <NavigationContainer>
+                <stack.Navigator headerMode={'none'}>
+                    <stack.Screen name='MainScreen' component={this.mainScreen}/>
+                    <stack.Screen name='ClientAddScreen' component={ClientAddScreen}/>
+                    <stack.Screen name='ClientsListScreen' component={ClientsListScreen}/>
+                    <stack.Screen name='DeliverPaymentScreen' component={DeliverPaymentScreen}/>
+                    <stack.Screen name='DeliverCheckScreen' component={DeliverCheckScreen}/>
+                    <stack.Screen name='ItemsListScreen' component={ItemsListScreen}/>
+                    <stack.Screen name='OrderAddItemsScreen' component={OrderAddItemsScreen}/>
+                    <stack.Screen name='OrderArticlesScreen' component={OrderArticlesScreen}/>
+                    <stack.Screen name='OrderConfirmsScreen' component={OrderConfirmsScreen}/>
+                    <stack.Screen name='SettingsScreen' component={SettingsScreen}/>
+                    <stack.Screen name='VisitDeliverScreen' component={VisitDeliverScreen}/>
+                    <stack.Screen name='VisitSalesScreen' component={VisitSalesScreen}/>
+                </stack.Navigator>
+            </NavigationContainer>
         );
     }
 }
