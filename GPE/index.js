053a90d1a5f55c6a2bb31790eabfc1289614a631
damagr <damiamartinez94@gmail.com>
2021-02-08 19:16:02 +0100

    NoTicket

--- GPE/index.js
+++ GPE/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import SettingsScreen from './screens/SettingsScreen';
+import MainScreen from './screens/MainScreen';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => SettingsScreen);
+AppRegistry.registerComponent(appName, () => MainScreen);
--- GPE/screens/ClientAddScreen.js
+++ GPE/screens/ClientAddScreen.js
@@ -1,12 +1,8 @@
-'use strict';
-
 import React, {Component} from 'react';
-import {Alert, View} from 'react-native';
+import {Alert, ScrollView, View} from 'react-native';
 import {NavigationBar} from '../components/NavigationBar';
 import {GPEInput} from '../components/GPEInput';
-import { axios, GPEApi, style } from '../components/GPEConst';
-import { ScrollView } from 'react-native-gesture-handler';
-
+import {axios, GPEApi, style} from '../components/GPEConst';
 
 export default class ClientAddScreen extends Component {
 
@@ -22,7 +18,6 @@ export default class ClientAddScreen extends Component {
             province: '',
             postalCode: '',
             city: '',
-
         };
     }
 
@@ -53,66 +48,70 @@ export default class ClientAddScreen extends Component {
     getProvince = (l) => {
         this.setState({province: l});
     };
-    onPressLeftIcon = () => {
-        console.log('Se pulsa el de la izquierda');
-    };
     onPressRightIcon = () => {
-        console.log('Se pulsa el de la derecha');
-        if(this.state.name==''||this.state.name==''||this.state.name==''||this.state.name==''||this.state.name==''||this.state.name==''||this.state.name==''||this.state.name==''||this.state.name==''){
+        if (this.state.name === '' || this.state.name === '' || this.state.name === '' || this.state.name === '' || this.state.name === '' || this.state.name === '' || this.state.name === '' || this.state.name === '' || this.state.name === '') {
             Alert.alert('You have empty inputs');
+        } else {
+            this.newClient();
         }
-        else{
-                this.initAxios();
-        }
-
-
     };
 
-
-    initAxios = () => {
-        let client ={
-            Name : this.state.name,
-            Address : this.state.address,
-            City : this.state.city ,
-            PostalCode :  this.state.postalCode,
-            Province : this.state.province ,
-            Phone : this.state.phone,
-            Email :  this.state.email,
-            NIF : this.state.nif ,
-            ContactName :this.state.barName ,
-
-        }
-        console.log('Se a enviado el post');
-        axios.post(GPEApi + 'Clients', client).then(console.log(axios.post(GPEApi + 'Clients', client)));
+    newClient = () => {
+        let client = {
+            Name: this.state.name,
+            Address: this.state.address,
+            City: this.state.city,
+            PostalCode: this.state.postalCode,
+            Province: this.state.province,
+            Phone: this.state.phone,
+            Email: this.state.email,
+            NIF: this.state.nif,
+            ContactName: this.state.contactName,
+        };
+            console.log(client.Name);
+            console.log(client.Address);
+            console.log(client.City);
+            console.log(client.PostalCode);
+            console.log(client.Province);
+            console.log(client.Phone);
+            console.log(client.Email);
+            console.log(client.NIF);
+            console.log(client.ContactName);
+        // axios.post(GPEApi + 'Clients', client).then(console.log(GPEApi + 'Clients', client));
     };
 
     render() {
         return (
             <View style={style.container}>
-                <NavigationBar leftIcon={'navigate-before'} leftIconSize={50} pressLeftIcon={this.onPressLeftIcon}
+                <NavigationBar leftIcon={'navigate-before'} leftIconSize={50}
+                               pressLeftIcon={() => this.props.navigation.goBack()}
                                pageName={'Add Client'} rightIcon={'done'} rightIconSize={50}
                                pressRightIcon={this.onPressRightIcon}/>
                 <View style={{alignItems: 'center'}}>
                     <ScrollView>
-                    <GPEInput title={'Name'} placeholder={'example name'} width='90%' height={5} marginTop='10%'
-                              onChangeText={this.getName}/>
-                    <GPEInput title={'Email'} placeholder={'example email'} width='90%' height={5} marginTop='5%'
-                              onChangeText={this.getEmail}/>
-                    <GPEInput title={'Contact Name'} placeholder={'example contact name'} width='90%' height={5} marginTop='5%'
-                              onChangeText={this.getConName}/>
-                    <GPEInput title={'NIF/NIE'} placeholder={'example nif/nie'} width='90%' height={5} marginTop='5%'
-                              onChangeText={this.getNif}/>
-                    <GPEInput title={'Phone number'} placeholder={'example phone number'} width='90%' height={5}
-                              marginTop='5%' onChangeText={this.getPhone}/>
-                    <GPEInput title={'City'} placeholder={'example city'} width='90%' height={5} marginTop='5%'
-                              onChangeText={this.getCity}/>
-                    <GPEInput title={'Postal Code'} placeholder={'example postal Code'} width='90%' height={5} marginTop='5%'
-                              onChangeText={this.getPostalCode}/>
-                    <GPEInput title={'Province'} placeholder={'example province'} width='90%' height={5} marginTop='5%'
-                              onChangeText={this.getProvince}/>
-                    <GPEInput title={'Adress'} placeholder={'example adress'} width='90%' height={5} marginTop='5%'
-                              onChangeText={this.getAddress}/>
-                              </ScrollView>
+                        <GPEInput title={'Name'} placeholder={'example name'} width='90%' height={5} marginTop='10%'
+                                  onChangeText={this.getName}/>
+                        <GPEInput title={'Email'} placeholder={'example email'} width='90%' height={5} marginTop='5%'
+                                  onChangeText={this.getEmail}/>
+                        <GPEInput title={'Contact Name'} placeholder={'example contact name'} width='90%' height={5}
+                                  marginTop='5%'
+                                  onChangeText={this.getConName}/>
+                        <GPEInput title={'NIF/NIE'} placeholder={'example nif/nie'} width='90%' height={5}
+                                  marginTop='5%'
+                                  onChangeText={this.getNif}/>
+                        <GPEInput title={'Phone number'} placeholder={'example phone number'} width='90%' height={5}
+                                  marginTop='5%' onChangeText={this.getPhone}/>
+                        <GPEInput title={'City'} placeholder={'example city'} width='90%' height={5} marginTop='5%'
+                                  onChangeText={this.getCity}/>
+                        <GPEInput title={'Postal Code'} placeholder={'example postal Code'} width='90%' height={5}
+                                  marginTop='5%'
+                                  onChangeText={this.getPostalCode}/>
+                        <GPEInput title={'Province'} placeholder={'example province'} width='90%' height={5}
+                                  marginTop='5%'
+                                  onChangeText={this.getProvince}/>
+                        <GPEInput title={'Adress'} placeholder={'example adress'} width='90%' height={5} marginTop='5%'
+                                  onChangeText={this.getAddress}/>
+                    </ScrollView>
                 </View>
             </View>
         );
