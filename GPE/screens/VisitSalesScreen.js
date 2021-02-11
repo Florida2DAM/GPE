801703e9f40ac62c443435d13e79d95ad5372620
damagr <damiamartinez94@gmail.com>
2021-02-11 12:43:35 +0100

    Comentado el código, indentado y modificadas las constantes, variables y estados según los estandares.

--- GPE/screens/VisitSalesScreen.js
+++ GPE/screens/VisitSalesScreen.js
@@ -1,52 +1,59 @@
-/* eslint-disable prettier/prettier */
-'use strict';
-import 'react-native-gesture-handler';
-import React, { Component } from 'react';
-import { FlatList, Pressable, View } from 'react-native';
+import React, {Component} from 'react';
+import {FlatList, Pressable, View} from 'react-native';
 import ClientCard from '../components/ClientCard';
-import { NavigationBar } from '../components/NavigationBar';
-import { GPEFilter } from '../components/GPEFilter';
-import { axios, GPEApi, style } from '../components/GPEConst';
+import {NavigationBar} from '../components/NavigationBar';
+import {GPEFilter} from '../components/GPEFilter';
+import {axios, GPEApi, style} from '../components/GPEConst';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default class VisitSalesScreen extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            employeeType: this.props.employeeType,
+            employee: {},
             allClients: [],
             clients: [],
             filter: '',
             juanjo: -1,
             toConfirm: false,
-            orderLines: []
+            orderLines: [],
         };
     }
 
+    // When we navigate to this screen we restore the object employee and get the clientsList
     componentDidMount() {
         this.getClients();
+        this.restoreEmployee();
     }
 
     componentDidUpdate() {
         if (this.props.route.params !== undefined) {
-            if (this.props.route.params.juanjo !== undefined && this.state.toConfirm === false) {                
+            if (this.props.route.params.juanjo !== undefined && this.state.toConfirm === false) {
                 if (this.props.route.params.juanjo !== this.state.juanjo) {
-                    this.setState({ toConfirm: true });
-                    this.setState({ orderLines: this.props.route.params.orderLines });
+                    this.setState({toConfirm: true});
+                    this.setState({orderLines: this.props.route.params.orderLines});
                 }
             }
         }
     }
 
+    // Restro from storage the employee object
+    async restoreEmployee() {
+        const jsonValue = await AsyncStorage.getItem('employee');
+        jsonValue != null ? this.setState({employee: JSON.parse(jsonValue)}) : null;
+    };
+
+    // Promise used to get all clients and save them into our states to show them as a clientsList
     getClients = () => {
         axios.get(GPEApi + 'Clients').then((response) => {
-            this.setState({ allClients: response.data });
-            this.setState({ clients: response.data });
+            this.setState({allClients: response.data});
+            this.setState({clients: response.data});
         });
     };
 
+    // Methods used to filter items in the screen using the GPEFiler component
     setFilter = (filter) => {
-        this.setState({ filter }, () => {
+        this.setState({filter}, () => {
             this.filter();
         });
     };
@@ -54,7 +61,7 @@ export default class VisitSalesScreen extends Component {
     filter = () => {
         let clientList = [];
         if (this.state.filter === '') {
-            this.setState({ clients: this.state.allClients });
+            this.setState({clients: this.state.allClients});
         } else {
             this.state.allClients.forEach(element => {
                 const filterText = this.state.filter.toUpperCase();
@@ -66,32 +73,35 @@ export default class VisitSalesScreen extends Component {
                     clientList.push(element);
                 }
             });
-            this.setState({ clients: clientList });
+            this.setState({clients: clientList});
         }
     };
 
     navigateJuanjo = (item) => {
         let screen;
-        if (this.state.toConfirm) screen = 'OrderConfirmsScreen';
-        else screen = 'OrderArticlesScreen';
-        this.setState({ toConfirm: false });
-        this.props.navigation.navigate(screen, { client: item, orderLines: this.state.orderLines });
-    }
+        if (this.state.toConfirm) {
+            screen = 'OrderConfirmsScreen';
+        } else {
+            screen = 'OrderArticlesScreen';
+        }
+        this.setState({toConfirm: false});
+        this.props.navigation.navigate(screen, {client: item, orderLines: this.state.orderLines});
+    };
 
     render() {
         return (
             <View style={style.container}>
                 <NavigationBar leftIcon={'arrow-back-ios'} leftIconSize={40} pageName={'Clients'} rightIcon={'add'}
-                    rightIconSize={50}
-                    pressLeftIcon={() => this.props.navigation.goBack()}
-                    pressRightIcon={() => this.props.navigation.navigate('ClientAddScreen')}
+                               rightIconSize={50}
+                               pressLeftIcon={() => this.props.navigation.goBack()}
+                               pressRightIcon={() => this.props.navigation.navigate('ClientAddScreen')}
                 />
-                <GPEFilter onChange={this.setFilter} />
-                <View style={[style.container, { flexDirection: 'column', flex: 5 }]}>
+                <GPEFilter onChange={this.setFilter}/>
+                <View style={[style.container, {flexDirection: 'column', flex: 5}]}>
                     <FlatList
                         data={this.state.clients}
                         keyExtractor={(item) => item.ClientId.toString()}
-                        renderItem={({ item, index }) => {
+                        renderItem={({item, index}) => {
                             return (
                                 <Pressable
                                     onPress={() => this.navigateJuanjo(item)}>
