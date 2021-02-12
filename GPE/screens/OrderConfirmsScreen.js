49c627b9080d94c47ef54741c33fbf23309e1f9f
minatr-png <otracuentaderrellenomas@gmail.com>
2021-02-12 19:22:02 +0100

    Bug visual corregido

--- GPE/screens/OrderConfirmsScreen.js
+++ GPE/screens/OrderConfirmsScreen.js
@@ -125,7 +125,7 @@ export default class OrderConfirmsScreen extends Component {
                     })}
                     pressRightIcon={this.changeVisibleConfirm} />
                 <Divider style={{ height: 10, backgroundColor: 'none' }} />
-                <View style={{marginLeft: '2%', marginRight: '2%'}}>
+                <View style={{ marginLeft: '2%', marginRight: '2%' }}>
                     <ContactInfo name={this.props.route.params.client.Name} dni={this.props.route.params.client.NIF}
                         change={() => {
                             this.props.navigation.navigate('ChangeClientScreen', {
@@ -133,21 +133,24 @@ export default class OrderConfirmsScreen extends Component {
                                 client: this.props.route.params.client,
                             });
                         }} />
-                    <Divider style={{ height: 10, backgroundColor: 'none' }} />
-                    <FlatList
-                        data={this.props.route.params.orderLines}
-                        keyExtractor={(item, index) => index.toString()}
-                        renderItem={({ item }) => {
-                            return (
+                </View>
+                <Divider style={{ height: 10, backgroundColor: 'none' }} />
+                <FlatList
+                    data={this.props.route.params.orderLines}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => {
+                        return (
+                            <View style={{ marginLeft: '2%', marginRight: '2%' }}>
                                 <ModifyQuantity orderLine={item} remove={() => {
                                     this.changeVisibleRemove();
                                     this.setState({ itemIdRemove: item.LineId });
                                 }}
                                     itemChange={this.updateInfo} />
-                            );
-                        }}
-                    />
-                </View>
+                            </View>
+                        );
+                    }}
+                />
+
                 <View style={{ alignItems: 'center' }}>
                     <GPELabel title="Total: " paddingLeft={'2%'} width={'50%'} marginBottom={'4%'}
                         content={this.state.total}
