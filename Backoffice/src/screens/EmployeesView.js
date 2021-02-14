ba8a4127fe1e5debde30f2f4b7f6c18848095976
damagr <damiamartinez94@gmail.com>
2021-02-14 18:57:36 +0100

    Indentado el código

--- Backoffice/src/screens/EmployeesView.js
+++ Backoffice/src/screens/EmployeesView.js
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { createRef, Fragment } from 'react';
+import {createRef, Fragment} from 'react';
 import '../App.css';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import { TabPanel, TabView } from 'primereact/tabview';
-import { Toast } from 'primereact/toast';
-import { axios, GPEApi } from '../components/GPEConst'
-import { GPEInput } from '../components/GPEInput';
-import { Dropdown } from 'primereact/dropdown';
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
+import {InputText} from 'primereact/inputtext';
+import {Button} from 'primereact/button';
+import {TabPanel, TabView} from 'primereact/tabview';
+import {Toast} from 'primereact/toast';
+import {axios, GPEApi} from '../components/GPEConst'
+import {GPEInput} from '../components/GPEInput';
+import {Dropdown} from 'primereact/dropdown';
 
 export class EmployeesView extends React.Component {
     constructor(props) {
@@ -45,9 +45,9 @@ export class EmployeesView extends React.Component {
                     deliverers.push(e.Name);
                 }
             });
-            this.setState({ deliverers });
-            this.setState({ employees: response.data });
-            this.setState({ allEmployees: response.data });
+            this.setState({deliverers});
+            this.setState({employees: response.data});
+            this.setState({allEmployees: response.data});
         })
 
     }
@@ -62,11 +62,11 @@ export class EmployeesView extends React.Component {
                 Enabled: false,
             }
             axios.post(GPEApi + 'Employees', employee).then(response => {
-                this.getEmployees();
-                this.clearInputs();
-                this.setState({ activeIndex: 0 });
-                this.GPEShowSuccess('Employee inserted');
-            }
+                    this.getEmployees();
+                    this.clearInputs();
+                    this.setState({activeIndex: 0});
+                    this.GPEShowSuccess('Employee inserted');
+                }
             )
         } else {
             this.GPEShowError('You have to introduce all fields')
@@ -83,11 +83,11 @@ export class EmployeesView extends React.Component {
                 Deliverer: this.state.deliverer
             }
             axios.put(GPEApi + 'Employees', emp).then(response => {
-                this.visibleHandler();
-                this.getEmployees();
-                this.clearInputs();
-                this.GPEShowSuccess('Employee updated');
-            }
+                    this.visibleHandler();
+                    this.getEmployees();
+                    this.clearInputs();
+                    this.GPEShowSuccess('Employee updated');
+                }
             )
         } else {
             this.GPEShowError('You have to introduce all fields')
@@ -97,9 +97,9 @@ export class EmployeesView extends React.Component {
     //Check the enabled field and shou a green button if is true and red button if is false
     btnActive = (rowData) => {
         return (<>{rowData.Enabled ?
-            <Button label='YES' onClick={() => this.changeEnabled(rowData)} className='p-button-success' />
+            <Button label='YES' onClick={() => this.changeEnabled(rowData)} className='p-button-success'/>
             :
-            <Button label='NO' onClick={() => this.changeEnabled(rowData)} className=' p-button-danger' />
+            <Button label='NO' onClick={() => this.changeEnabled(rowData)} className=' p-button-danger'/>
         }
         </>)
     }
@@ -110,43 +110,43 @@ export class EmployeesView extends React.Component {
 
     //Take the value and save it in the state
     nameHandler = (e) => {
-        this.setState({ name: e.target.value });
+        this.setState({name: e.target.value});
     };
-    
+
     //Take the value and save it in the state
     delivererHandler = (e) => {
-        this.setState({ deliverer: e.target.value });
+        this.setState({deliverer: e.target.value});
     };
-    
+
     //Take the value and save it in the state
     employeeIdHandler = (e) => {
-        this.setState({ employeeId: e.target.value });
+        this.setState({employeeId: e.target.value});
     };
 
     //Take the value and save it in the state
     typeHandler = (e) => {
         if (e.target.value == 'Deliverer') {
-            this.setState({ type: e.target.value });
-            this.setState({ deliverer: '' });
+            this.setState({type: e.target.value});
+            this.setState({deliverer: ''});
         } else {
-            this.setState({ type: e.target.value });
+            this.setState({type: e.target.value});
         }
     };
 
     //Take the value and save it in the state
     enabledHandler = (e) => {
-        this.setState({ enabled: e.target.value });
+        this.setState({enabled: e.target.value});
     };
     //Show and hide the modify screen
     visibleHandler = () => {
-        this.setState({ visible: !this.state.visible });
+        this.setState({visible: !this.state.visible});
     }
 
     //Clear inputs value
     clearInputs = () => {
-        this.setState({ name: '' });
-        this.setState({ type: '' });
-        this.setState({ deliverer: '' })
+        this.setState({name: ''});
+        this.setState({type: ''});
+        this.setState({deliverer: ''})
     }
 
     checkSalesMan = () => {
@@ -170,21 +170,21 @@ export class EmployeesView extends React.Component {
     //tTake the information of the actual employye and save it in the state for use it later
     showInputs = (rowData) => {
         this.visibleHandler();
-        this.setState({ employeeId: rowData.EmployeeId });
-        this.setState({ name: rowData.Name });
-        this.setState({ enabled: rowData.Enabled });
+        this.setState({employeeId: rowData.EmployeeId});
+        this.setState({name: rowData.Name});
+        this.setState({enabled: rowData.Enabled});
     }
 
     //Button used for go to the modify screen
     changePage = (rowData) => {
         return <Button label='Modify' icon='pi pi-pencil' onClick={() => this.showInputs(rowData)}
-            className='p-button-secondary p-mr-2'
-            style={{ backgroundColor: '#86AEC2' }} />
+                       className='p-button-secondary p-mr-2'
+                       style={{backgroundColor: '#86AEC2'}}/>
     }
 
     //Take the value and save it in the state
     filterHandler = (e) => {
-        this.setState({ filter: e.target.value }, () => {
+        this.setState({filter: e.target.value}, () => {
             this.filter();
         });
     };
@@ -193,7 +193,7 @@ export class EmployeesView extends React.Component {
     filter = () => {
         let employeeList = [];
         if (this.state.filter === '') {
-            this.setState({ employees: this.state.allEmployees });
+            this.setState({employees: this.state.allEmployees});
 
         } else {
             this.state.allEmployees.forEach(element => {
@@ -205,7 +205,7 @@ export class EmployeesView extends React.Component {
                     employeeList.push(element);
                 }
             });
-            this.setState({ employees: employeeList });
+            this.setState({employees: employeeList});
         }
     };
 
@@ -216,8 +216,8 @@ export class EmployeesView extends React.Component {
                 employeeList.push(element);
             }
         });
-        this.setState({ employees: employeeList }, () => {
-            this.setState({ show: !this.state.show })
+        this.setState({employees: employeeList}, () => {
+            this.setState({show: !this.state.show})
         });
     };
 
@@ -228,86 +228,89 @@ export class EmployeesView extends React.Component {
                 employeeList.push(element);
             }
         });
-        this.setState({ employees: employeeList }, () => {
-            this.setState({ show: !this.state.show })
+        this.setState({employees: employeeList}, () => {
+            this.setState({show: !this.state.show})
         });
     };
 
     GPEShowError = (error) => {
-        this.GPEAlert.current.show({ severity: 'error', summary: 'Error', detail: error, life: 3000 });
+        this.GPEAlert.current.show({severity: 'error', summary: 'Error', detail: error, life: 3000});
     }
-    
+
     GPEShowSuccess = (detailValue) => {
-        this.GPEAlert.current.show({ severity: 'success', summary: 'Done', detail: detailValue, life: 3000 });
+        this.GPEAlert.current.show({severity: 'success', summary: 'Done', detail: detailValue, life: 3000});
     }
 
     render() {
         return (
             <Fragment>
-                <Toast ref={this.GPEAlert} />
+                <Toast ref={this.GPEAlert}/>
                 <TabView activeIndex={this.state.activeIndex}
-                    onTabChange={(e) => this.setState({ activeIndex: e.index })}>
+                         onTabChange={(e) => this.setState({activeIndex: e.index})}>
                     <TabPanel header='Employees Filter'>
                         {this.state.visible === true ? <div>
-                            <div className='flex-center'>
-                                <GPEInput onChange={this.filterHandler} />
-                                <Button label='Refresh' icon='pi pi-refresh' onClick={this.resetStates}
-                                    className='p-button-secondary p-mr-2'
-                                    style={{ backgroundColor: '#86AEC2' }} />
-                                {this.state.show ?
-                                    <Button label='Show Enable' onClick={this.showEnable}
-                                        className='p-button-secondary p-mr-2' icon='pi pi-eye'
-                                        style={{ backgroundColor: '#86AEC2' }} /> :
-                                    <Button label='Show Disable' onClick={this.showDisable}
-                                        className='p-button-secondary p-mr-2' icon='pi pi-eye'
-                                        style={{ backgroundColor: '#86AEC2' }} />
-                                }
-                            </div>
-                            <div>
-                                <DataTable value={this.state.employees}>
-                                    <Column style={{ textAlign: 'center' }} field='EmployeeId'
-                                        header='EmployeeId' />
-                                    <Column style={{ textAlign: 'center' }} field='Name' header='Name' />
-                                    <Column style={{ textAlign: 'center' }} field='Type'
-                                        header='Type' />
-                                    <Column style={{ textAlign: 'center' }} field='Deliverer'
-                                        header='Deliverer' />
-                                    <Column body={this.btnActive} style={{ textAlign: 'center' }}
-                                        field='Enabled' header='Enabled' />
-                                    <Column style={{ textAlign: 'center' }} body={this.changePage}
-                                        field='Modify' header='Modify' />
-                                </DataTable>
-                            </div>
-                        </div> :
-                            <div> <InputText value={this.state.employeeId} onChange={this.employeeIdHandler} disabled
-                                placeholder='ID' />
+                                <div className='flex-center'>
+                                    <GPEInput onChange={this.filterHandler}/>
+                                    <Button label='Refresh' icon='pi pi-refresh' onClick={this.resetStates}
+                                            className='p-button-secondary p-mr-2'
+                                            style={{backgroundColor: '#86AEC2'}}/>
+                                    {this.state.show ?
+                                        <Button label='Show Enable' onClick={this.showEnable}
+                                                className='p-button-secondary p-mr-2' icon='pi pi-eye'
+                                                style={{backgroundColor: '#86AEC2'}}/> :
+                                        <Button label='Show Disable' onClick={this.showDisable}
+                                                className='p-button-secondary p-mr-2' icon='pi pi-eye'
+                                                style={{backgroundColor: '#86AEC2'}}/>
+                                    }
+                                </div>
+                                <div>
+                                    <DataTable value={this.state.employees}>
+                                        <Column style={{textAlign: 'center'}} field='EmployeeId'
+                                                header='EmployeeId'/>
+                                        <Column style={{textAlign: 'center'}} field='Name' header='Name'/>
+                                        <Column style={{textAlign: 'center'}} field='Type'
+                                                header='Type'/>
+                                        <Column style={{textAlign: 'center'}} field='Deliverer'
+                                                header='Deliverer'/>
+                                        <Column body={this.btnActive} style={{textAlign: 'center'}}
+                                                field='Enabled' header='Enabled'/>
+                                        <Column style={{textAlign: 'center'}} body={this.changePage}
+                                                field='Modify' header='Modify'/>
+                                    </DataTable>
+                                </div>
+                            </div> :
+                            <div><InputText value={this.state.employeeId} onChange={this.employeeIdHandler} disabled
+                                            placeholder='ID'/>
                                 <InputText value={this.state.name} onChange={this.nameHandler}
-                                    placeholder='Name' className={this.state.name == '' && 'p-invalid p-d-block'} />
+                                           placeholder='Name'
+                                           className={this.state.name == '' && 'p-invalid p-d-block'}/>
                                 <Dropdown value={this.state.type} options={this.state.types}
-                                    placeholder='Select Type' onChange={this.typeHandler}
-                                    className={this.state.type == '' && 'p-invalid p-d-block'} />
-                                {this.state.type == 'Salesman' && <Dropdown value={this.state.deliverer} options={this.state.deliverers}
-                                    placeholder='Select Deliverer' onChange={this.delivererHandler}
-                                    className={this.state.type == '' && 'p-invalid p-d-block'} />}
+                                          placeholder='Select Type' onChange={this.typeHandler}
+                                          className={this.state.type == '' && 'p-invalid p-d-block'}/>
+                                {this.state.type == 'Salesman' &&
+                                <Dropdown value={this.state.deliverer} options={this.state.deliverers}
+                                          placeholder='Select Deliverer' onChange={this.delivererHandler}
+                                          className={this.state.type == '' && 'p-invalid p-d-block'}/>}
                                 <Button label='Modify' icon='pi pi-send' onClick={this.updateEmployee}
-                                    className='p-button-secondary p-mr-2'
-                                    style={{ backgroundColor: '#77FF94', color: 'black' }} />
+                                        className='p-button-secondary p-mr-2'
+                                        style={{backgroundColor: '#77FF94', color: 'black'}}/>
 
                             </div>}
                     </TabPanel>
                     <TabPanel header='New Employees'>
                         <div className='flex-center'>
                             <InputText value={this.state.name} onChange={this.nameHandler}
-                                placeholder='Name' className={this.state.name == '' && 'p-invalid p-d-block'} />
+                                       placeholder='Name' className={this.state.name == '' && 'p-invalid p-d-block'}/>
                             <Dropdown value={this.state.type} options={this.state.types}
-                                placeholder='Select Type' onChange={this.typeHandler}
-                                className={this.state.type == '' && 'p-invalid p-d-block'} />
-                            {this.state.type == 'Salesman' && <Dropdown value={this.state.deliverer} options={this.state.deliverers}
-                                placeholder='Select Deliverer' onChange={this.delivererHandler}
-                                className={this.state.type == '' && 'p-invalid p-d-block'} />}
+                                      placeholder='Select Type' onChange={this.typeHandler}
+                                      className={this.state.type == '' && 'p-invalid p-d-block'}/>
+                            {this.state.type == 'Salesman' &&
+                            <Dropdown value={this.state.deliverer} options={this.state.deliverers}
+                                      placeholder='Select Deliverer' onChange={this.delivererHandler}
+                                      className={this.state.type == '' && 'p-invalid p-d-block'}/>}
                             <Button label=' New Lot' icon='pi pi-plus-circle' onClick={this.addEmployee}
-                                className='p-button-secondary p-mr-2'
-                                style={{ backgroundColor: '#77FF94', color: 'black' }} />
+                                    className='p-button-secondary p-mr-2'
+                                    style={{backgroundColor: '#77FF94', color: 'black'}}/>
                         </div>
                     </TabPanel>
                 </TabView>
